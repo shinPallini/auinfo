68ac8180315c87155d60e71f3e166f27090198a6
pallini <shinpall1ni@gmail.com>
2022-06-28 12:07:10 +0000

    仮のスラッシュコマンドとmain関数を追加

---
+++ roles/custom-command2.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"log"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/shinPallini/discordgox"
+)
+
+func init() {
+	embed := discordgox.NewMessageEmbed(
+		discordgox.SetTitle("Embed_New"),
+		discordgox.SetDescription("New Embed_New"),
+		discordgox.SetColor(0x111111),
+		discordgox.SetEmbedField(
+			discordgox.NewList(
+				discordgox.NewMessageEmbedField(
+					discordgox.SetEmbedFieldName("field11"),
+					discordgox.SetEmbedFieldValue("value11"),
+				),
+			),
+		),
+	)
+	response := discordgox.NewInteractionResponse(
+		discordgox.SetType(discordgo.InteractionResponseChannelMessageWithSource),
+		discordgox.SetData(discordgox.NewInteractionResponseData(
+			discordgox.SetContent("custom interaction2"),
+			discordgox.SetEmbed(
+				discordgox.NewList(
+					discordgox.NewMessageEmbed(
+						discordgox.SetTitle("Embed"),
+						discordgox.SetDescription("New Embed"),
+						discordgox.SetColor(0x111111),
+						discordgox.SetEmbedField(
+							discordgox.NewList(
+								discordgox.NewMessageEmbedField(
+									discordgox.SetEmbedFieldName("field1"),
+									discordgox.SetEmbedFieldValue("value1"),
+								),
+							),
+						),
+					),
+					embed,
+				),
+			),
+			discordgox.SetComponent(
+				discordgox.NewList[discordgo.MessageComponent](
+					discordgox.NewActionsRow(
+						discordgox.SetLinkButton(
+							"Custom link",
+							"https://go.dev/doc/tutorial/call-module-code",
+						),
+					),
+				),
+			),
+		)),
+	)
+
+	discordgox.AddCommand(
+		&discordgo.ApplicationCommand{
+			Name:        "custom-command2",
+			Description: "Custom command2",
+		},
+		func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			s.InteractionRespond(i.Interaction, response)
+		},
+	)
+	log.Println(response.Data)
+	log.Println(discordgox.Commands)
+}
---
+++ roles/main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+	"github.com/shinPallini/discordgox"
+)
+
+var (
+	GuildID        string
+	BotToken       string
+	RemoveCommands bool = true
+)
+
+var s *discordgo.Session
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	GuildID = os.Getenv("GUILDID")
+	BotToken = os.Getenv("BOTTOKEN")
+}
+
+func init() {
+	var err error
+	s, err = discordgo.New("Bot " + BotToken)
+	if err != nil {
+		log.Fatalf("Invalid bot parameters: %v", err)
+	}
+}
+
+func main() {
+	commandList := discordgox.Commands
+	commandHandlers := discordgox.CommandHandlers
+	componentHandlers := discordgox.ComponentHandlers
+
+	// s.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+	// 	log.Printf("Logged in as: %v#%v", s.State.User.Username, s.State.User.Discriminator)
+	// })
+
+	func(s *discordgo.Session) {
+		s.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+			log.Printf("Logged in as: %v#%v", s.State.User.Username, s.State.User.Discriminator)
+		})
+	}(s)
+	// Components are part of interactions, so we register InteractionCreate handler
+	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		switch i.Type {
+		case discordgo.InteractionApplicationCommand:
+			if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
+				h(s, i)
+			}
+		case discordgo.InteractionMessageComponent:
+			if h, ok := componentHandlers[i.MessageComponentData().CustomID]; ok {
+				h(s, i)
+			}
+		}
+	})
+	err := s.Open()
+	if err != nil {
+		log.Fatalf("Cannot open the session: %v", err)
+	}
+	defer s.Close()
+
+	log.Println("Adding commands...")
+	log.Println(commandList)
+	registeredCommands := make([]*discordgo.ApplicationCommand, len(commandList))
+	for i, v := range commandList {
+		cmd, err := s.ApplicationCommandCreate(s.State.User.ID, GuildID, v)
+		if err != nil {
+			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+		}
+		registeredCommands[i] = cmd
+	}
+
+	defer s.Close()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	log.Println("Press Ctrl+C to exit")
+	<-stop
+
+	if RemoveCommands {
+		log.Println("Removing commands...")
+		// // We need to fetch the commands, since deleting requires the command ID.
+		// // We are doing this from the returned commands on line 375, because using
+		// // this will delete all the commands, which might not be desirable, so we
+		// // are deleting only the commands that we added.
+		// registeredCommands, err := s.ApplicationCommands(s.State.User.ID, *GuildID)
+		// if err != nil {
+		// 	log.Fatalf("Could not fetch registered commands: %v", err)
+		// }
+
+		for _, v := range registeredCommands {
+			err := s.ApplicationCommandDelete(s.State.User.ID, GuildID, v.ID)
+			if err != nil {
+				log.Panicf("Cannot delete '%v' command: %v", v.Name, err)
+			}
+		}
+	}
+
+	log.Println("Gracefully shutting down.")
+
+}
